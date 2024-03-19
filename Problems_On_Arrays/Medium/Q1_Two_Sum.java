ab55ec3af7a8dab26d539ee2ff9d34d6ce77050d
Arnabg_9 <arnabghorai15@gmail.com>
2024-03-19 19:33:33 +0530

    Q5

---
+++ Problems_On_Arrays/Medium/Q1_Two_Sum.java
@@ -0,0 +1,24 @@
+package Problems_On_Arrays.Medium;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Q1_Two_Sum {
+    public static void main(String[] args) {
+        int[] arr = {2,7,11,15};
+        System.out.println(Arrays.toString(twoSum(arr, 9)));
+    }
+    
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], i);
+        }
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(target-nums[i]) && i != map.get(target-nums[i])){
+                return new int[]{i, map.get(target-nums[i])};
+            }
+        }
+        return new int []{-1, -1};
+    }
+}
---
+++ Problems_On_Arrays/Medium/Q5_Largest_SubArray_With_Sum_k.java
@@ -0,0 +1,28 @@
+package Problems_On_Arrays.Medium;
+
+public class Q5_Largest_SubArray_With_Sum_k {
+    public static void main(String[] args) {
+        int k = 3;
+        int arr[] = {1, 2, 3, 1, 1, 1, 1};
+        System.out.println(longestSubarrayWithSumK(arr, k));
+    }
+    //optimal -- using two pointer approach
+    public static int longestSubarrayWithSumK(int []a, long k) {
+        int n = a.length-1;
+        long sum = 0;
+        int maxLen = 0;
+        int right = 0;
+        int left = 0;
+        while(right<=n){
+            sum+=a[right];
+            while(left<=right && sum>k){
+                sum-=a[left++];
+            }
+            if(sum==k){
+                maxLen = Math.max(maxLen, right-left+1);
+            }
+            right++;
+        }
+        return maxLen;
+    }
+}
